1b9e879178af30681d0bb4fb54c94379adf5145e
Jeffrey Bekker <jef.bekker@gmail.com>
2024-07-25 19:10:24 +0200

    Modified some apps and added a new template.

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name="home")
+    path('home', views.home, name="home"),
+    path('todos/', views.todos, name="Todos"),
 ]
